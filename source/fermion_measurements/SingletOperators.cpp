a634ff5f45f30dfa0fd39a5eb6217b07b6fbaa9e
Stefano Piemonte <nspiemonte@gmail.com>
2015-10-07 14:36:53 +0000

    bugfix singlet operators

--- source/fermion_measurements/SingletOperators.cpp
+++ source/fermion_measurements/SingletOperators.cpp
@@ -19,13 +19,14 @@ void SingletOperators::execute(environment_t& environment) {
 	}
 	diracOperator->setLattice(environment.getFermionLattice());
 
+	long_real_t A11 = 0.;
+	long_real_t A10 = 0.;
+	long_real_t A00 = 0.;
+
 	for (unsigned int step = 0; step < max_step; ++step) {
 		this->generateRandomNoise(randomNoise);
 		biConjugateGradient->solve(squareDiracOperator, randomNoise, tmp_square);
 		diracOperator->multiply(tmp, tmp_square);
-		long_real_t A11 = 0.;
-		long_real_t A10 = 0.;
-		long_real_t A00 = 0.;
 
 #pragma omp parallel for reduction(+:A00,A10,A11)
 		for (int site = 0; site < environment.getFermionLattice().localsize; ++site) {
@@ -45,9 +46,9 @@ void SingletOperators::execute(environment_t& environment) {
 
 
 	long_real_t volume = environment.gaugeLinkConfiguration.getLayout().globalVolume;
-	A00 = A00/volume;
-	A10 = A10/volume;
-	A11 = A11/volume;
+	A00 = A00/(volume*max_step);
+	A10 = A10/(volume*max_step);
+	A11 = A11/(volume*max_step);
 
 	std::cout << "Vediamo di che morte dobbiamo morire: " << A00 << " " << A10 << " " << A11 << std::endl;
 }
