557d4792bdbca882adee4303063d87ae8c76026d
Stefano Piemonte <nspiemonte@gmail.com>
2016-09-13 07:55:00 +0000

    update restart sim

--- python_utils/restartsimulations.py
+++ python_utils/restartsimulations.py
@@ -13,9 +13,9 @@ inputfile = open(sys.argv[1],'r')
 
 filedata = inputfile.read()
 
-patterninput = re.search('input_directory_configurations\ *=\ *([a-zA-Z\_\/0-9\-]+)',filedata)
-patternoutput = re.search('output_directory_configurations\ *=\ *([a-zA-Z\_\/0-9\-]+)',filedata)
-patternoutputname = re.search('output_configuration_name\ *=\ *([a-zA-Z\_\/0-9\-]+)',filedata)
+patterninput = re.search('input_directory_configurations\ *=\ *(.+)',filedata)
+patternoutput = re.search('output_directory_configurations\ *=\ *(.+)',filedata)
+patternoutputname = re.search('output_configuration_name\ *=\ *(.+)',filedata)
 patternoutputfile = re.search('output_name\ *=\ *(.+_)([0-9]+)',filedata)
 
 patternjump = re.search('Output,\ *([0-9]+)',filedata)
@@ -44,6 +44,15 @@ if numberLastConf == 0:
 			tmp = int(pattern.group(1))
 			if tmp > numberLastConf:
 				numberLastConf = tmp
+
+if numberLastConf == 0:
+        print "Warning, no old configurations found, trying with new su3 format"
+        for namefile in os.listdir(patternoutput.group(1)):
+		pattern = re.search(patternoutputname.group(1)+'\.config\-([0-9]+)',namefile)
+                if pattern != None:
+                        tmp = int(pattern.group(1))
+                        if tmp > numberLastConf:
+                                numberLastConf = tmp
 
 print "Restarting from configuration number: " + str(numberLastConf)
 
